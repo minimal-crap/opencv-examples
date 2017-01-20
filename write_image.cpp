e88d2f9cf7b90227011f5674a49213fb6fd0cb0c
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-01-20 19:42:27 +0530

    modify write_image.cpp to add pixel randomizer

--- write_image.cpp
+++ write_image.cpp
@@ -1,20 +1,33 @@
 #include<iostream>
 #include<cstdlib>
 
+#include "boost/random.hpp"
+#include "boost/generator_iterator.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
 using namespace std;
 using namespace cv;
 
+
+int get_random_number(){
+	typedef boost::mt19937 RNGType;
+	RNGType rng;
+	boost::uniform_int<> random_int(0, 255);
+	boost::variate_generator< RNGType, boost::uniform_int<> > pixel_randomizer(rng, random_int);
+	return pixel_randomizer();
+};
+
 Vec3b return_random_pixel(){
+	typedef boost::mt19937 RNGType;
+        RNGType rng;
+        boost::uniform_int<> random_int(0, 255);
+        boost::variate_generator< RNGType, boost::uniform_int<> > pixel_randomizer(rng, random_int);
+
 	Vec3b random_pixel;
-	srand(time(NULL));
-	random_pixel[0] = rand() % 255;
-	srand(time(NULL));
-	random_pixel[1] = rand() % 255;
-	srand(time(NULL));
-	random_pixel[2] = rand() % 255;
+	random_pixel[0] = pixel_randomizer();
+	random_pixel[1] = pixel_randomizer();
+	random_pixel[2] = pixel_randomizer();
 	return random_pixel;
 };
 
